2ef57ff38082885789e46caa0a89fe51dee25f11
ingunn03 <ingunn.elmo@gmail.com>
2026-09-07 13:36:40 +0200

    endring oppgave

--- Trinnskatt.java
+++ Trinnskatt.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class Trinnskatt {
-    public Trinnskatt() {
-    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
